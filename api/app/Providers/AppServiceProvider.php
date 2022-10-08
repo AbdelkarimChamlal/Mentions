a03bbb75a92f318b6816ccc39ae0c035966aa6ee
AbdelkarimChamlal <abdelkarim.chamlal@gmail.com>
2022-10-08 22:41:23 +0100

    update scopes

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -28,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
             $scopes = [
                 "notifications",
                 "read:user",
+                "admin:repo_hook",
+                "admin:org_hook"
             ];
             return new Github(config('services.github.client_id'), config('services.github.client_secret'), config('services.github.redirect_uri'), $scopes);
         });
