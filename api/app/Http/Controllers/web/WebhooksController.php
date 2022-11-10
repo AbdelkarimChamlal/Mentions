21d5cbddbdce072775b70a7a580c9847820815d6
AbdelkarimChamlal <abdelkarim.chamlal@gmail.com>
2022-11-10 23:18:29 +0100

    add log for github

--- api/app/Http/Controllers/web/WebhooksController.php
+++ api/app/Http/Controllers/web/WebhooksController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 use App\services\GithubServices;
 use App\Http\Controllers\Controller;
 use App\services\SlackServices;
+use Illuminate\Support\Facades\Log;
 
 class WebhooksController extends Controller
 {
     public function githubWebhooks(Request $request,Github $github)
     {
+        Log::info($request->all());
         $github_service = new GithubServices($github);
         $github_service->handle_webhook($request);
         return response()->json(['success' => true]);
